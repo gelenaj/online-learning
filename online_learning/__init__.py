54693f7ec3764c27524f768b36b89c7ae36e4ccd
gelenaj <gelenaj@gmail.com>
2017-12-27 02:11:26 -0500

    added production settings

---
+++ online_learning/__init__.py
@@ -0,0 +1,4 @@
+from online_learning.settings *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
--- online_learning/settings.py
+++ online_learning/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vs6bl0+z-8q352@(k1jv@l%!i#)3&=4e5s#gwm8!lvr%!d6tr^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
