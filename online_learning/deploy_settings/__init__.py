d82dd50da7a1c24ec1a8bf3e6745e940d5f66da8
gelenaj <gelenaj@gmail.com>
2017-12-27 02:38:52 -0500

    moved init file

---
+++ online_learning/deploy_settings/__init__.py
@@ -0,0 +1,11 @@
+from online_learning.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS=[
+    'localhost',
+    '127.0.0.1:8000',
+    'gj-online-learning.herokuapp.com',
+    '*.herokuapp.com',
+]
